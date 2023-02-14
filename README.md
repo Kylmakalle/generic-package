f9b89eb780695ddcb3ac6444bbe87cad230e589e
Sergey Akentev <sergey.akentev@mapbox.com>
2023-02-14 16:12:18 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# generic-package
+
+A description of this package.
---
+++ Tests/generic-packageTests/generic_packageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import generic_package
+
+final class generic_packageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(generic_package().text, "Hello, World!")
+    }
+}
